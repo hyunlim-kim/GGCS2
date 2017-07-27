f73309389ccb7418000ce51ac789890b1a67c9f9
joy <lcocvce@naver.com>
2017-07-27 14:40:18 +0900

    I Love Joy

--- app/src/main/java/org/androidtown/calendar/month/ExpenseActivity.java
+++ app/src/main/java/org/androidtown/calendar/month/ExpenseActivity.java
@@ -2,8 +2,17 @@ package org.androidtown.calendar.month;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ExpenseActivity extends AppCompatActivity {
+
+    TextView dateView;
+    int year;   //년
+    int month;  //월
+    int date;   //일
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
