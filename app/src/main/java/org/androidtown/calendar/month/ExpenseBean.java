746b1f326ba7bd0d9d8126152fda334d5796c8aa
wuju96 <wuju96@naver.com>
2017-07-31 15:41:20 +0900

    ExpenseBean 수정했어용

--- app/src/main/java/org/androidtown/calendar/month/ExpenseBean.java
+++ app/src/main/java/org/androidtown/calendar/month/ExpenseBean.java
@@ -8,25 +8,41 @@ import java.io.Serializable;
 
 public class ExpenseBean implements Serializable {
 
-    private String payment; // 지출액
-    private String detail; // 사용내역
-    private String etc; // 카드/상점
+    private String sex; // 지출액
+    private String money; // 사용내역
+    private String detail; // 카드/상점
+    private String place; //사용처
+    private String memo; //메모
+    private String money_id;
+    private String date; //날짜
+    private String user_id; //아이디
 
 
-    public ExpenseBean(String payment, String detail, String etc) {
-        this.payment = payment;
+    public ExpenseBean(String sex, String detail, String money, String place, String memo, String money_id, String date, String user_id) {
+        this.sex = sex;
+        this.money = money;
         this.detail = detail;
-        this.etc = etc;
-
+        this.place = place;
+        this.memo = memo;
+        this.money_id = money_id;
+        this.date = date;
+        this.user_id = user_id;
     }
 
-
-    public String getPayment() {
-        return payment;
+    public String getSex() {
+        return sex;
     }
 
-    public void setPayment(String payment) {
-        this.payment = payment;
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
     }
 
     public String getDetail() {
@@ -37,14 +53,43 @@ public class ExpenseBean implements Serializable {
         this.detail = detail;
     }
 
-    public String getEtc() {
-        return etc;
+    public String getPlace() {
+        return place;
     }
 
-    public void setEtc(String etc) {
-        this.etc = etc;
+    public void setPlace(String place) {
+        this.place = place;
     }
 
+    public String getMemo() {
+        return memo;
+    }
 
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
 
+    public String getMoney_id() {
+        return money_id;
+    }
+
+    public void setMoney_id(String money_id) {
+        this.money_id = money_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
 }
